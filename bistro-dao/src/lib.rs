9690552bdc94c1cc944d1772d01a8857b526655c
Ansgar Sachs <ansgar.sa@googlemail.com>
2020-08-06 19:50:24 +0200

    feat: #8 add dao for retrieving menus

--- bistro-dao/src/lib.rs
+++ bistro-dao/src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+extern crate bistro_contract;
+extern crate arangors;
+
+use arangors::{Connection, Database};
+use arangors::client::reqwest::ReqwestClient;
+use bistro_contract::menu::Menu;
+
+pub async fn connect() -> Option<Connection> {
+    let conn = Connection::establish_basic_auth("http://localhost:8529", "bistrouser", "bistropassword")
+        .await
+        .unwrap();
+
+    Some(conn)
+}
+
+pub async fn get_bistro_db() -> Option<Database<ReqwestClient>> {
+    let conn = connect().await.unwrap();
+    let db = conn.db("bistro").await.unwrap();
+
+    Some(db)
+}
+
+pub async fn get_menus() -> Option<Vec<Menu>> {
+    let db = get_bistro_db().await.unwrap();
+    let menus: Vec<Menu> = db
+        .aql_str("FOR menu IN menus RETURN menu")
+        .await
+        .unwrap();
+
+    Some(menus)
 }
