db27bf1236851e3049f8096346b4bff5ac9dfc3f
Ansgar Sachs <ansgar.sa@googlemail.com>
2021-03-04 22:21:49 +0100

    refactor: combine data query params into range

--- src/menu/endpoints.rs
+++ src/menu/endpoints.rs
@@ -2,7 +2,7 @@ use crate::config::AppConfig;
 use crate::menu::repository::{query_all_menus, query_menu_by_id, query_menus_by_range};
 use crate::middleware::api_response::ApiResponse;
 use crate::middleware::arango_pool::ArangoPool;
-use crate::middleware::date_query::DateQueryParam;
+use crate::middleware::date_query::{DateRangeQueryParam};
 
 use rocket::http::{RawStr, Status};
 use rocket_contrib::json;
@@ -35,14 +35,13 @@ pub async fn get_all_menus(pool: ArangoPool, config: State<'_, AppConfig>) -> Ap
 ///
 /// Todo from and to should be combined --> max range == 7 days as restriction
 ///
-#[get("/?<from>&<to>")]
+#[get("/?<range..>")]
 pub async fn get_all_menus_by_date_range(
-    from: DateQueryParam,
-    to: DateQueryParam,
+    range: DateRangeQueryParam,
     pool: ArangoPool,
     config: State<'_, AppConfig>
 ) -> ApiResponse {
-    let menu_result = query_menus_by_range(*from, *to, pool, config.inner()).await;
+    let menu_result = query_menus_by_range(range.from, range.to, pool, config.inner()).await;
 
     match menu_result {
         Ok(menus) => ApiResponse {
--- src/middleware/date_query.rs
+++ src/middleware/date_query.rs
@@ -1,26 +1,38 @@
 use chrono::NaiveDate;
-use rocket::http::RawStr;
-use rocket::request::FromFormValue;
-use std::ops::Deref;
+use rocket::request::{FromQuery, Query};
 
-// TODO Create a DateRange Extractor
-pub struct DateQueryParam(NaiveDate);
-
-impl<'v> FromFormValue<'v> for DateQueryParam {
-    type Error = &'v RawStr;
-
-    fn from_form_value(form_value: &'v RawStr) -> Result<DateQueryParam, &'v RawStr> {
-        let decoded = form_value.url_decode().map_err(|_| form_value)?;
-        if let Ok(date) = NaiveDate::parse_from_str(&decoded, "%Y-%m-%d") {
-            return Ok(DateQueryParam(date));
-        }
-        Err(form_value)
-    }
+/// Query Parameter for a date range
+///
+/// This param is only parsed, if both a 'from' and a 'to'
+/// query param is set. If one of these dates cannot be parsed
+/// with the format 'YYYY-MM-DD', then no date range can be used.
+///
+#[derive(Copy, Clone, Debug)]
+pub struct DateRangeQueryParam {
+    pub from: NaiveDate,
+    pub to: NaiveDate
 }
 
-impl Deref for DateQueryParam {
-    type Target = NaiveDate;
-    fn deref(&self) -> &NaiveDate {
-        &self.0
+
+impl<'v> FromQuery<'v> for DateRangeQueryParam {
+    type Error = &'static str;
+
+    fn from_query(query: Query<'v>) -> Result<Self, Self::Error> {
+        let (from_param, to_param) = query
+            .fold((None, None), |(from, to), curr| {
+                match curr.key.as_str() {
+                     "from" => (Some(curr), to),
+                    "to" => (from, Some(curr)),
+                    _ => (from, to)
+                }
+            });
+
+        let from = from_param.ok_or("Query param 'from' is missing")?.value.url_decode().map_err(|_| "Query param 'from' is malformed")?;
+        let to = to_param.ok_or("Query param 'to' is missing")?.value.url_decode().map_err(|_| "Query param 'to' is malformed")?;
+
+        Ok(DateRangeQueryParam {
+            from: NaiveDate::parse_from_str(&from, "%Y-%m-%d").map_err(|_| "Query param 'from' has to be in format YYYY-MM-DD")?,
+            to: NaiveDate::parse_from_str(&to, "%Y-%m-%d").map_err(|_| "Query param 'from' has to be in format YYYY-MM-DD")?,
+        })
     }
 }
